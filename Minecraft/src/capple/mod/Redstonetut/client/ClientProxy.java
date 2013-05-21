f84a9f78e52aa99e8a37d633b544961c25d08847
Capple234 <marten69windowslive@windowslive.com>
2013-05-21 21:20:32 +0300

    Create ClientProxy.java

---
+++ Minecraft/src/capple/mod/Redstonetut/client/ClientProxy.java
@@ -0,0 +1,31 @@
+package capple.mod.Redstonetut.client;
+
+import capple.mod.Redstonetut.*;
+import capple.mod.Redstonetut.common.CommonProxy;
+import net.minecraftforge.client.MinecraftForgeClient;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class ClientProxy extends CommonProxy
+
+{
+  
+	@Override
+	public void registerRenderers(){
+		MinecraftForgeClient.preloadTexture(Redstone_1);
+		MinecraftForgeClient.preloadTexture(Redstone_2);
+		MinecraftForgeClient.preloadTexture(Obsidian_1);
+		MinecraftForgeClient.preloadTexture(Obsidian_2);
+		MinecraftForgeClient.preloadTexture(ITEMS);
+	}
+	
+	
+	@Override
+	public int addArmor(String armor){
+			return RenderingRegistry.addNewArmourRendererPrefix(armor);
+	}
+}
+	
+
