4fae02a1be1e3f801fa0f19d179f80ce51d8a243
Capple234 <marten69windowslive@windowslive.com>
2013-05-21 21:45:28 +0300

    Create PanicKey.java

---
+++ Minecraft/src/capple/mod/Redstonetut/common/PanicKey.java
@@ -0,0 +1,39 @@
+package capple.mod.Redstonetut.common;
+
+import java.util.EnumSet;
+
+import net.minecraft.client.settings.KeyBinding;
+import cpw.mods.fml.client.registry.KeyBindingRegistry.KeyHandler;
+import cpw.mods.fml.common.TickType;
+
+public class PanicKey extends KeyHandler {
+  
+	private EnumSet tickTypes = EnumSet.of(TickType.CLIENT);
+	
+	public PanicKey(KeyBinding[] keyBindings, boolean[] repeatings)
+    {
+            super(keyBindings, repeatings);
+    }
+    @Override
+    public String getLabel()
+    {
+            return "PanicKey";
+    }
+    @Override
+    public void keyDown(EnumSet<TickType> types, KeyBinding kb, boolean tickEnd, boolean isRepeat)
+    {
+    	
+    }
+    @Override
+    public void keyUp(EnumSet<TickType> types, KeyBinding kb, boolean tickEnd)
+    {
+    	
+    }
+    @Override
+    public EnumSet<TickType> ticks()
+    {
+            return tickTypes;
+    }
+}
+
+
