bbdd12d1a3cedeb8badf90b1ba3370bb7c47727d
Capple234 <marten69windowslive@windowslive.com>
2013-06-07 16:14:06 +0300

    Update CommonProxy.java

--- Minecraft/src/capple/mod/Redstonetut/common/CommonProxy.java
+++ Minecraft/src/capple/mod/Redstonetut/common/CommonProxy.java
@@ -2,19 +2,24 @@ package capple.mod.Redstonetut.common;
 
 public abstract class CommonProxy
 {
-  
+	
 	public static String Redstone_1 = "mods.RedstoneTUT.textures.armor.Redstone_1.png";
 	public static String Redstone_2 = "mods.RedstoneTUT.textures.armor.Redstone_1.png";
 	public static String Obsidian_1 = "mods.RedstoneTUT.textures.armor.Obsidian_1.png";
 	public static String Obsidian_2 = "mods.RedstoneTUT.textures.armor.Obsidian_2.png";
 	public static String ITEMS = "mods.RedstoneTUT.textures.items.png";
+	public ServerTickHandler tickHandlerServer = null;
 
 	abstract public void registerRenderers();
-		
+	
 	
 	
 	abstract public int addArmor(String armor);
-	
-	
+
+
+	public void registerTickHandlers() {
+		
+		
+	}
 
 }
