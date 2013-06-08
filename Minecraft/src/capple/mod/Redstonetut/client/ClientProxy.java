89563966e9aac6bcc98b2666d3f8a116de25e48f
Capple234 <marten69windowslive@windowslive.com>
2013-06-08 16:29:38 +0300

    Update ClientProxy.java

--- Minecraft/src/capple/mod/Redstonetut/client/ClientProxy.java
+++ Minecraft/src/capple/mod/Redstonetut/client/ClientProxy.java
@@ -7,11 +7,11 @@ import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-@SideOnly(Side.CLIENT)
+
 public class ClientProxy extends CommonProxy
 
 {
-  
+	
 	@Override
 	public void registerRenderers(){
 		MinecraftForgeClient.preloadTexture(Redstone_1);
